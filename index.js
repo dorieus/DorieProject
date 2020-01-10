9c46c21f3572fe0d136876dc528f3303fda2556a
Dorie Us <usdarina@gmail.com>
2020-01-10 09:39:36 +0200

    created function getRecommendation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
 const film = {};
+
 film.name = "Deadpool";
 film.description =
   "A wisecracking mercenary gets experimented on and becomes immortal but ugly, and sets out to track down the man who ruined his looks. Armed with his new abilities and a dark, twisted sense of humor, Deadpool hunts down the man who nearly destroyed his life. Wade Wilson is a small-time mercenary. He meets Vanessa and falls in love. Life is idyllic until one day he is diagnosed with terminal cancer.";
@@ -11,18 +12,21 @@ film.actors = "Ryan Reynolds, Morena Baccarin, T.J. Miller";
 film.rate = 8.0;
 film.watch = confirm(`Have you watched ${film.name}?`);
 
-if ((film.watch = true)) {
+if (film.watch) {
   alert("Chimichanga!");
 } else {
   alert("Bang-Bang!");
 }
 
-function changeRating(newRating = 0) {
-  newRating.quest = confirm (`Did you like ${film.name}?`);
+function getRecommendation() {
+  const newRating = (function ask() {
+    const n = prompt(`Rate the movie from 1 to 5 ${film.name}`);
+    return isNaN(n) || +n > 5 || +n < 1 ? ask() : +n;
+  })();
   film.rate = newRating;
-  if ((newRating.quest = true)) {
-    alert((newRating.quest = 9.0));
-  } else {
-    alert((newRating.quest = 7.0));
-  }
+  if (newRating === 1) alert("Don't watch it!");
+  else if (newRating === 2) alert("It is boring");
+  else if (newRating === 3) alert("I will not watch it again");
+  else if (newRating === 4) alert("It is good movie");
+  else if (newRating === 5) alert("It is the best movie I have ever watched!");
 }
